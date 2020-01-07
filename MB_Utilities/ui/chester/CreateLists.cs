705937158d7c7aa453b209b4ff976bc986d3490c
Steve <stevefrazee123@gmail.com>
2020-01-07 15:25:30 -0500

    add file/folder state checks

--- MB_Utilities/ui/chester/CreateLists.cs
+++ MB_Utilities/ui/chester/CreateLists.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using OfficeOpenXml;
 
 namespace MB_Utilities.ui.chester
 {
     public partial class CreateLists : UserControl
     {
         private const int DELETE_ROWS_WARNING = 0;
+
+        // state of missing list
+        private const int MISSING_LIST_READY = 0;
+        private const int MISSING_LIST_PATH_EMPTY = 1;
+        private const int MISSING_LIST_NOT_FOUND = 2;
+        private const int MISSING_LIST_INCORRECT = 3;
+
+        // state of log file
+        private const int FILE_READY = 4;
+        private const int FILE_PATH_EMPTY = 5;
+        private const int FILE_NOT_FOUND = 6;
+
+        // state of folder
+        private const int FOLDER_READY = 7;
+        private const int FOLDER_PATH_EMPTY = 8;
+        private const int FOLDER_NOT_FOUND = 9;
+        private const int FOLDER_IS_EMPTY = 10;
+        private const int CONTAINS_BAD_FILE = 11;
+
+        private const int CANNOT_SAVE_MISSING_LIST = 12;
 
         public CreateLists()
         {
@@ -60,7 +83,7 @@ namespace MB_Utilities.ui.chester
         {
             disableUI();
 
-            DialogResult warningSelection = DialogResult.No;
+            DialogResult warningSelection = DialogResult.Yes;
             if (deleteRowsCheckBox.Checked)
             {
                 warningSelection = showWarning(DELETE_ROWS_WARNING);
@@ -68,7 +91,25 @@ namespace MB_Utilities.ui.chester
 
             if (warningSelection == DialogResult.Yes)
             {
-                // do stuff
+                int missingListState = getMissingListState();
+                int fileState = getFileState();
+                int folderState = getFolderState();
+                if (missingListState != MISSING_LIST_READY)
+                {
+                    showErrorMessage(missingListState);
+                }
+                else if (fileState != FILE_READY)
+                {
+                    showErrorMessage(fileState);
+                }
+                else if (folderState != FOLDER_READY)
+                {
+                    showErrorMessage(folderState);
+                }
+                else
+                {
+                    // do stuff
+                }
             }
 
             enableUI();
@@ -76,6 +117,94 @@ namespace MB_Utilities.ui.chester
 
 
         /************* UTILITY FUNCTIONS ******************/
+        private int getMissingListState()
+        {
+            if (string.IsNullOrEmpty(missingListPathField.Text))
+            {
+                return MISSING_LIST_PATH_EMPTY;
+            }
+            else if (!File.Exists(missingListPathField.Text))
+            {
+                return MISSING_LIST_NOT_FOUND;
+            }
+            else if (!correctMissingList())
+            {
+                return MISSING_LIST_INCORRECT;
+            }
+            return MISSING_LIST_READY;
+        }
+
+        private int getFileState()
+        {
+            if (string.IsNullOrEmpty(logFilePathField.Text))
+            {
+                return FILE_PATH_EMPTY;
+            }
+            else if (!File.Exists(logFilePathField.Text))
+            {
+                return FILE_NOT_FOUND;
+            }
+            return FILE_READY;
+        }
+
+        private int getFolderState()
+        {
+            if (string.IsNullOrEmpty(folderPathField.Text))
+            {
+                return FOLDER_PATH_EMPTY;
+            }
+            else if (!Directory.Exists(folderPathField.Text))
+            {
+                return FOLDER_NOT_FOUND;
+            }
+            else if (!Directory.EnumerateFiles(folderPathField.Text, "*.pdf").Any())
+            {
+                return FOLDER_IS_EMPTY;
+            }
+            else if (!fileNamesAreGood())
+            {
+                return CONTAINS_BAD_FILE;
+            }
+            return FOLDER_READY;
+        }
+
+        private bool fileNamesAreGood()
+        {
+            DirectoryInfo folder = new DirectoryInfo(folderPathField.Text);
+            FileInfo[] files = folder.GetFiles("*.pdf");
+            foreach (FileInfo file in files)
+            {
+                try
+                {
+                    string[] splitFileName = file.Name.Split('.');
+                    int chartNum = Int32.Parse(splitFileName[0]);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is FormatException || ex is OverflowException)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool correctMissingList()
+        {
+            FileInfo missingListPath = new FileInfo(missingListPathField.Text);
+            using (ExcelPackage packageMissing = new ExcelPackage(missingListPath))
+            using (ExcelWorksheet worksheetMissing = packageMissing.Workbook.Worksheets[1])
+            {
+                string title = worksheetMissing.Cells[1, 1].GetValue<string>();
+                if (title == "CT Unbilled Report")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         private void disableUI()
         {
             chooseMissingListBTN.Enabled = false;
@@ -107,6 +236,48 @@ namespace MB_Utilities.ui.chester
                     return MessageBox.Show(createListMessage, title, buttons);
                 default:
                     return DialogResult.No;
+            }
+        }
+
+        private void showErrorMessage(int error)
+        {
+            switch (error)
+            {
+                case MISSING_LIST_PATH_EMPTY:
+                    MessageBox.Show("Please select the CT missing list");
+                    return;
+                case MISSING_LIST_NOT_FOUND:
+                    MessageBox.Show("The CT missing list could not be found.");
+                    return;
+                case MISSING_LIST_INCORRECT:
+                    MessageBox.Show("The missing list you chose is not the CT missing list.");
+                    return;
+                case FILE_PATH_EMPTY:
+                    MessageBox.Show("Please select a file.");
+                    return;
+                case FILE_NOT_FOUND:
+                    MessageBox.Show("The file you selected could not be found.");
+                    return;
+                case FOLDER_PATH_EMPTY:
+                    MessageBox.Show("Please select a folder.");
+                    return;
+                case FOLDER_NOT_FOUND:
+                    MessageBox.Show("The folder you selected could not be found.");
+                    return;
+                case FOLDER_IS_EMPTY:
+                    MessageBox.Show("There are no pdf files in the selected folder");
+                    return;
+                case CONTAINS_BAD_FILE:
+                    MessageBox.Show("There was a problem with one or more file names. Please rename the files and try again.");
+                    return;
+                case CANNOT_SAVE_MISSING_LIST:
+                    MessageBox.Show("It looks like the missing list is open somewhere else.\n\n" +
+                        "The missing list will not be updated.\n\n" +
+                        "Close the missing list and try again.");
+                    return;
+                default:
+                    MessageBox.Show("An unspecified error occurred.");
+                    return;
             }
         }
     }
