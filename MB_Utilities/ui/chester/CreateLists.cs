822484eedd11a986494e2c2cf16f78cf3157c0c7
Steve <stevefrazee123@gmail.com>
2020-01-28 06:51:09 -0500

    don't overwrite file_lists.docx

--- MB_Utilities/ui/chester/CreateLists.cs
+++ MB_Utilities/ui/chester/CreateLists.cs
@@ -557,6 +557,12 @@ namespace MB_Utilities.ui.chester
                 }
 
                 object fileName = folderPathField.Text + "\\file_lists.docx";
+                int fileNum = 1;
+                while(File.Exists(fileName.ToString()))
+                {
+                    fileName = folderPathField.Text + "\\file_lists_" + fileNum.ToString() + ".docx";
+                    fileNum++;
+                }
                 document.SaveAs2(ref fileName);
                 document.Close();
                 application.Quit();
